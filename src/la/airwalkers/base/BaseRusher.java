847d758444acb8dc2847faac5588722acace46bd
airwalkers <airwalkers@163.com>
2017-03-03 23:26:49 +0800

    Date:2017.3.3 Detail:add Model.House

--- src/la/airwalkers/base/BaseRusher.java
+++ src/la/airwalkers/base/BaseRusher.java
@@ -1,11 +1,15 @@
 package la.airwalkers.base;
 import la.airwalkers.base.webdriver.Driver;
+import la.airwalkers.model.House;
 import org.openqa.selenium.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by liupf on 2017/3/1.
  */
-public class BaseRusher {
+public abstract class BaseRusher {
     protected WebDriver driver;
     protected String baseUrl;
 
@@ -14,8 +18,20 @@ public class BaseRusher {
         driver = Driver.getChromeDriver();
     }
 
+    public List<House> getHouses(int start, int end) {
+        List<House> houses = read(start, end);
+        quit();
+        return houses;
+    }
+
+    public abstract List<House> read(int start, int end);
+
+    public void quit() {
+        driver.quit();
+    }
+
     public static void main(String[] args) {
-        BaseRusher rusher = new BaseRusher("http://su.58.com/sushizhongxin/ershoufang/0/pn1/");
-        rusher.driver.navigate().to(rusher.baseUrl);
+//        BaseRusher rusher = new BaseRusher("http://su.58.com/sushizhongxin/ershoufang/0/pn1/");
+//        rusher.driver.navigate().to(rusher.baseUrl);
     }
 }
